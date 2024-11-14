3b160024e6dbc22368d88d07ea6c32a645e20995
Alex Koval <al3xander.koval@gmail.com>
2024-11-14 15:05:42 +0100

    add required fields to token payments

--- lib/tiptop_pay/namespaces/tokens.rb
+++ lib/tiptop_pay/namespaces/tokens.rb
@@ -3,12 +3,21 @@
 module TiptopPay
   module Namespaces
     class Tokens < Base
+      AmountNotProvided = Class.new(StandardError)
+      TokenNotProvided = Class.new(StandardError)
+
       def charge(attributes)
+        attributes.fetch(:amount) { raise AmountNotProvided.new("amount attribute is required") }
+        attributes.fetch(:token) { raise TokenNotProvided.new("token attribute is required") }
+
         response = request(:charge, attributes)
         Transaction.new(response[:model])
       end
 
       def auth(attributes)
+        attributes.fetch(:amount) { raise AmountNotProvided.new("amount attribute is required") }
+        attributes.fetch(:token) { raise TokenNotProvided.new("token attribute is required") }
+
         response = request(:auth, attributes)
         Transaction.new(response[:model])
       end
