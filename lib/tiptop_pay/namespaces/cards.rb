0dd9271468bc49851b48fdfe0ecb56105fe8c4c5
Alex Koval <al3xander.koval@gmail.com>
2024-11-14 15:01:04 +0100

    add required fields to cards

--- lib/tiptop_pay/namespaces/cards.rb
+++ lib/tiptop_pay/namespaces/cards.rb
@@ -3,18 +3,35 @@
 module TiptopPay
   module Namespaces
     class Cards < Base
+      AmountNotProvided = Class.new(StandardError)
+      IpNotProvided = Class.new(StandardError)
+      CardCryptogramPacketNotProvided = Class.new(StandardError)
+      TransactionIdNotProvided = Class.new(StandardError)
+      PaResNotProvided = Class.new(StandardError)
+
       def charge(attributes)
+        attributes.fetch(:amount) { raise AmountNotProvided.new("amount attribute is required") }
+        attributes.fetch(:ip_address) { raise IpNotProvided.new("ip_address attribute is required") }
+        attributes.fetch(:card_cryptogram_packet) { raise CardCryptogramPacketNotProvided.new("card_cryptogram_packet attribute is required") }
+
         response = request(:charge, attributes)
         instantiate(response[:model])
       end
 
       def auth(attributes)
+        attributes.fetch(:amount) { raise AmountNotProvided.new("amount attribute is required") }
+        attributes.fetch(:ip_address) { raise IpNotProvided.new("ip_address attribute is required") }
+        attributes.fetch(:card_cryptogram_packet) { raise CardCryptogramPacketNotProvided.new("card_cryptogram_packet attribute is required") }
+
         response = request(:auth, attributes)
         instantiate(response[:model])
       end
 
       def post3ds(attributes)
-        response = request(:post3ds, attributes)
+        id = attributes.fetch(:id) { raise TransactionIdNotProvided.new("id attribute is required") }
+        pa_res = attributes.fetch(:pa_res) { raise PaResNotProvided.new("pa_res attribute is required") }
+
+        response = request(:post3ds, transaction_id: id, pa_res:)
         instantiate(response[:model])
       end
 
